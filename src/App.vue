bb7a71d01f3e4d736a5faee727662369e17809d2
Grapevine <jinsw2001@gmail.com>
2018-10-12 17:06:31 +0900

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <TodoHeader></TodoHeader>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+        <TodoFooter></TodoFooter>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoHeader from './components/TodoHeader'
+    import TodoList from './components/TodoList'
+    import TodoInput from './components/TodoInput'
+    import TodoFooter from './components/TodoFooter'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                todoItems: []
+            }
+        },
+        methods: {
+            // removeOneItem(todoItem, index) {
+            //     localStorage.removeItem(todoItem.item);
+            //     this.todoItems.splice(index, 1);
+            // },
+            // toggleOneItem(todoItem, index) {
+            //     this.todoItems[index].completed = !this.todoItems[index].completed;
+            //     localStorage.removeItem(todoItem.item);
+            //     localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+            // },
+            // clearAllItem() {
+            //     localStorage.clear();
+            //     this.todoItems = [];
+            // }
+        },
+        name: 'app',
+        components: {
+            TodoHeader,
+            TodoInput,
+            TodoList,
+            TodoFooter
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        text-align: center;
+        background-color: #F6f6f6;
+    }
+
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+
+    button {
+        border-style: groove;
+    }
+
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+    }
+
 </style>
